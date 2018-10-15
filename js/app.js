62f7e9fcc224dc71fc1f3ff51c8abc68a946c041
JennAh <jennah@jenns-air.gateway.ht.net>
2018-10-15 00:10:19 -1000

    populated films list

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-
+//person4 code
 const person4 = new XMLHttpRequest();
 
 function reqListener () {
@@ -22,7 +22,7 @@ home4.open("GET", "https://swapi.co/api/planets/1/");
 home4.send();
 
 
-//person 14 code
+//person14 code
 
 const person14 = new XMLHttpRequest();
 
@@ -44,3 +44,122 @@ person14.send();
 species14var.addEventListener("load", species14);
 species14var.open("GET", "https://swapi.co/api/species/1/");
 species14var.send();
+
+
+//filmList
+
+//first film
+
+const filmListElem = new XMLHttpRequest(); //film request
+const filmListUnordered = document.getElementById('filmList'); //unordered film list
+filmListUnordered.innerHTML += '<h1>Films</h2>'
+const firstFilm = document.createElement('li'); //first film list
+const film1 = document.getElementById('film1title');
+const filmListPlanets = document.createElement('ul');//planets unordered list
+
+function filmOne () {
+  firstFilm.className = "film";
+  firstFilm.id = 'film1title';
+  filmListUnordered.innerHTML += JSON.parse(filmListElem['response']).results[0].title;
+};
+
+function filmOnePlanets () {
+  firstFilm.innerHTML += JSON.parse(filmListElem['response']).results[0].title;
+  console.log('testing this function');
+}
+
+filmListElem.addEventListener("load", filmOne);
+filmListElem.addEventListener("load", filmOnePlanets);
+filmListElem.open("GET", "https://swapi.co/api/films/");
+filmListElem.send();
+
+
+//second film
+const secFilm = document.createElement('li'); //first film list
+//films.appendChild(filmListUnordered);
+
+function filmTwo () {
+  secFilm.className = "film";
+  //firstFilm.id = 'film1title';
+  filmListUnordered.innerHTML += '<br>' + JSON.parse(filmListElem['response']).results[1].title;
+};
+
+filmListElem.addEventListener("load", filmTwo);
+//filmListElem.open("GET", "https://swapi.co/api/films/");
+//filmListElem.send();
+
+//third film
+
+const thirdFilm = document.createElement('li'); //first film list
+//films.appendChild(filmListUnordered);
+
+function filmThree () {
+  thirdFilm.className = "film";
+  //firstFilm.id = 'film1title';
+  filmListUnordered.innerHTML += '<br>' + JSON.parse(filmListElem['response']).results[2].title;
+};
+
+filmListElem.addEventListener("load", filmThree);
+//filmListElem.open("GET", "https://swapi.co/api/films/");
+//filmListElem.send();
+
+
+//fourth film
+
+const fourFilm = document.createElement('li'); //first film list
+//films.appendChild(filmListUnordered);
+
+function filmFour () {
+  fourFilm.className = "film";
+  //firstFilm.id = 'film1title';
+  filmListUnordered.innerHTML += '<br>' + JSON.parse(filmListElem['response']).results[3].title;
+};
+
+filmListElem.addEventListener("load", filmFour);
+//filmListElem.open("GET", "https://swapi.co/api/films/");
+//filmListElem.send();
+
+//fifth film
+
+const fiveFilm = document.createElement('li'); //first film list
+films.appendChild(filmListUnordered);
+
+function filmFive () {
+  fiveFilm.className = "film";
+  //firstFilm.id = 'film1title';
+  filmListUnordered.innerHTML += '<br>' + JSON.parse(filmListElem['response']).results[4].title;
+};
+
+filmListElem.addEventListener("load", filmFive);
+//filmListElem.open("GET", "https://swapi.co/api/films/");
+//filmListElem.send();
+
+//sixth film
+
+const sixthFilm = document.createElement('li'); //first film list
+films.appendChild(filmListUnordered);
+
+function filmSix () {
+  sixthFilm.className = "film";
+  //firstFilm.id = 'film1title';
+  filmListUnordered.innerHTML += '<br>' + JSON.parse(filmListElem['response']).results[5].title;
+};
+
+filmListElem.addEventListener("load", filmSix);
+//filmListElem.open("GET", "https://swapi.co/api/films/");
+//filmListElem.send();
+
+//seventh film
+
+const sevFilm = document.createElement('li'); //first film list
+films.appendChild(filmListUnordered);
+
+function filmSeven () {
+  sevFilm.className = "film";
+  //firstFilm.id = 'film1title';
+  filmListUnordered.innerHTML += '<br>' + JSON.parse(filmListElem['response']).results[6].title;
+};
+
+filmListElem.addEventListener("load", filmSeven);
+//filmListElem.open("GET", "https://swapi.co/api/films/");
+//filmListElem.send();
